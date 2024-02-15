7b0d99a065cf3a3f92447214ad21b26fef928d5d
NikitaClark <81870653+NikitaClark@users.noreply.github.com>
2024-02-14 20:14:26 -0500

    full update

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SignUp extends Activity {
@@ -10,5 +13,13 @@ public class SignUp extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        Button continueButton = findViewById(R.id.continueButton);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
