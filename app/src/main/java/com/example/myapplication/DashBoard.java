85aa69f0f67fe14cca725a507454e7fc9b68ab93
NikitaClark <81870653+NikitaClark@users.noreply.github.com>
2024-03-20 12:19:48 -0400

    update on the dashboard

--- app/src/main/java/com/example/myapplication/DashBoard.java
+++ app/src/main/java/com/example/myapplication/DashBoard.java
@@ -24,6 +24,7 @@ public class DashBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+      
         // Initialize elements
         headerProfile = findViewById(R.id.header_profile);
         avatarImage = findViewById(R.id.avatar_image);
